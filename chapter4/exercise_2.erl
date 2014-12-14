5603fbafdd00438d865281eec7ac32d2c74a1421
myang <myang@localhost.localdomain>
2014-12-14 11:35:14 +0800

    change code style

--- chapter4/exercise_2.erl
+++ chapter4/exercise_2.erl
@@ -8,10 +8,10 @@ my_tuple_to_list(T) ->
 
 my_tuple_to_list_iter(T, Index, L) ->
   if 
-  Index > erlang:tuple_size(T) -> 
-    lists:reverse(L);		
-  true ->
-    my_tuple_to_list_iter(T, Index + 1, [element(Index , T) | L])
+    Index > erlang:tuple_size(T) -> 
+      lists:reverse(L);		
+    true ->
+      my_tuple_to_list_iter(T, Index + 1, [element(Index , T) | L])
   end.
 
 
