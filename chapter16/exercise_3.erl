ac58ca231d08b625989e55fc10040a6261e3af87
myang <myang199088@163.com>
2015-01-10 18:18:09 +0800

    exercise

---
+++ chapter16/exercise_3.erl
@@ -0,0 +1,16 @@
+-module(exercise_3).
+-export([md5/1]).
+-define(BYTES_PER_READ, 1024).
+
+md5(FileName) ->
+  {ok, FileHandle} = file:open(FileName, [read, binary, raw]),
+  md5(FileHandle, 0, erlang:md5_init()).  
+
+md5(FileHandle, Location, Context) ->
+  case file:pread(FileHandle, Location, ?BYTES_PER_READ) of
+    {ok, Data} ->
+      NewContext = erlang:md5_update(Context, Data),
+      md5(FileHandle, Location + ?BYTES_PER_READ, NewContext);
+    eof ->
+      erlang:md5_final(Context)
+    end.
